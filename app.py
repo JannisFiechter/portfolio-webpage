cf80a5c01387f9da7934fe83b37fee715ecf0817
Jannis Fiechter <jf.fiechter@outlook.com>
2025-07-07 18:13:34 +0200

    text gets now pulled from a json file

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+
+    # Load data from JSON file
+    data = load_json_data("experience-projects.json")
+    print(data)  # Debugging line to check the loaded data
+    return render_template("index.html", data=data)
+
+### Functions
+# Load json data for projects, skills, and experience
+def load_json_data(filename):
+    with open(filename) as file:
+        data = json.load(file)
+    return data
 
 if __name__ == "__main__":
     app.run(debug=True)
